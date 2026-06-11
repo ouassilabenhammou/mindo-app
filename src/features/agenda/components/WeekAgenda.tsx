a192a77eb0afd1e8fccf71530b220f8d8d70569b
ouassilabenhammou <509114@student.fontys.nl>
2026-06-11 22:04:38 +0200

    feat: vertaal weekdagen naar Nederlands

--- src/features/agenda/components/WeekAgenda.tsx
+++ src/features/agenda/components/WeekAgenda.tsx
@@ -2,15 +2,38 @@ import { useState } from "react";
 import { Text, View } from "react-native";
 import { WeeklyCalendar } from "react-native-simple-weekly-calendar";
 
+import dayjs from "dayjs";
+import "dayjs/locale/nl";
+
+dayjs.locale("nl");
+
 export default function WeekAgenda() {
   const vandaag = new Date().toISOString().split("T")[0];
   const [selectedDate, setSelectedDate] = useState(vandaag);
+
   return (
     <View>
-      <Text>Week Agenda</Text>
       <WeeklyCalendar
+        // Omzetten van dag naar Nederlands
+
+        dayHeaderComponent={({ dayOfWeek, theme }) => {
+          const dag = dayOfWeek.replace(".", "");
+
+          return (
+            <Text style={{ color: theme.dayHeaderTextColor }}>
+              {dag.charAt(0).toUpperCase() + dag.slice(1)}
+            </Text>
+          );
+        }}
+        monthComponent={({ weekFirstDate, theme }) => {
+          const maand = new Intl.DateTimeFormat("nl-NL", {
+            month: "short",
+            year: "numeric",
+          }).format(new Date(weekFirstDate));
+          return <Text style={{ color: theme.monthTextColor }}>{maand}</Text>;
+        }}
         // Style aanpassen van kalender
-        theme={{ calendarBackgroundColor: "none" }}
+        theme={{ calendarBackgroundColor: "transparent" }}
         initialDate={vandaag}
         markedDays={[{ date: vandaag }]}
         nextComponent={() => <View style={{ width: 40, height: 40 }} />}
